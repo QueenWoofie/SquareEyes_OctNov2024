65e96208c07b974377adfe6ca2fd77b57eca174c
Thea <142231299+QueenWoofie@users.noreply.github.com>
2024-11-12 03:23:57 +0100

    #2
    Checkout Completed
    
    Next steps -
    * Create a checkout confirmation page (required page for assignment)
    * Confirm that the steps listed in "important features" have been met
    * Add about us page
    * Add contact page
    
    * Potentially create a "movies" page with filtering options (optional)

--- js/checkout.js
+++ js/checkout.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const checkoutList = document.getElementById('checkout-list');
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const removeFromCartSound = new Audio('../assets/audio/remove-from-cart.mp3');
 
     let totalPrice = 0;
 
-    cart.forEach(item => {
+    cart.forEach((item, index) => {
         const checkoutItem = document.createElement('div');
         checkoutItem.classList.add('checkout-item');
 
         checkoutItem.innerHTML = `
-                <div class="cart-item">
+                <div class="incart-item">
                     <div class="product">
-                        <src="${item.image.url}" alt="${item.image.alt || 'Product Image'}">
+                        <img src="${item.image.url}" alt="${item.image.alt || 'Product Image'}">
                         <div class="details">
                             <h2>${item.title}</h2>
                             <p>${item.genre}</p>
@@ -23,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     </div>
                     <div class="price-section">
                         <p>${item.price},-</p>
+                        <button class="delete-item trash icon" data-index="${index}"></button>
                     </div>
                 </div>
         `;
@@ -32,6 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
         totalPrice += item.price;
     });
 
+    totalPrice = totalPrice.toFixed(2);
+
     const totalPriceSection = document.createElement('div');
     totalPriceSection.classList.add('total-price-section');
     totalPriceSection.innerHTML = `
@@ -39,4 +43,18 @@ document.addEventListener('DOMContentLoaded', () => {
     `;
 
     checkoutList.appendChild(totalPriceSection);
+
+    document.querySelectorAll('.delete-item').forEach(button => {
+        button.addEventListener('click', deleteItem);
+    });
+
+    function deleteItem(event) {
+        const index = event.target.getAttribute('data-index');
+        cart.splice(index, 1);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        removeFromCartSound.play();
+        setTimeout (() => {
+            location.reload();
+         }, 1000);
+    }
 });
